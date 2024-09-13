470beb8e467cd40b6dddd154f41bc4b5356ca332
Jakub Smid <jakubsmid22@gmail.com>
2024-09-13 19:43:11 +0200

    Added Example Project To Docs Page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ titles }) => {
   return (
     <header className="bg-[#161F73] text-[#AEFF00] p-4">
       <div className="text-2xl font-bold">⚽ Czech Football API</div>
-      <nav className="mt-2 space-x-4">
+      <nav className="mt-2 space-x-4 w-full text-sm md:text-base">
         <a
           target="_blank"
           href="https://rapidapi.com/jakubsmid22/api/czech-football-api"
@@ -27,10 +27,17 @@ const Header = ({ titles }) => {
         </a>
         <a
           target="_blank"
+          href="https://github.com/jakubsmid22/czech-league-teams-app"
+          className="hover:text-white"
+        >
+          Example Project
+        </a>
+        <a
+          target="_blank"
           href="https://github.com/jakubsmid22/czech-football-api"
           className="hover:text-white"
         >
-          <FaGithub className="inline" />
+          <FaGithub className="inline text-2xl" />
         </a>
       </nav>
     </header>
